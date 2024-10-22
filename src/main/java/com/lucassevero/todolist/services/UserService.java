0869096c456338f49b70d3d34ab1f18c5f4c2f82
lucas.severo <lucasgabriel31032006@gmail.com>
2024-10-22 09:32:40 -0400

    estudando java spring boot

---
+++ src/main/java/com/lucassevero/todolist/services/UserService.java
@@ -0,0 +1,53 @@
+package com.lucassevero.todolist.services;
+
+import com.lucassevero.todolist.models.User;
+import com.lucassevero.todolist.repositories.TaskRepository;
+import com.lucassevero.todolist.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    //@Autowired para injetar dependencias
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    //busca o usuario pelo seu id, ele verifica a existencia do user no banco, e se não existir, retorna uma execption
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+            "User not found! ID: " + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Could not delete this Entity!");
+        }
+    }
+
+}
